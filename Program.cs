471e8874ca09479c501570d405b7b655f0fee60f
Matej Vodopivc <matej.vodopivc@gmail.com>
2018-08-21 23:38:00 +0200

    Init.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet.RaspberryPi.App1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
